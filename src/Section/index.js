7afa192a949e99ad5f8f833dc4ec599c8cc72247
Maciej.Konitz <maciej.konitz@wp.pl>
2023-04-16 19:40:11 +0200

    h3 className section header change to h2

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-    <h3 className="section__header">{title}</h3>
+    <h2 className="section__header">{title}</h2>
     {extraHeaderContent}
-    <div className="section_body">
+    <div className="section__body">
       {body}
     </div>
   </section>
